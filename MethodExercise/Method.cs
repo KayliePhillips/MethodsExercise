ed3584a86d2c5b04385c30734e8578fd91a6763c
Kaylie Phillips <kaylie.travel@gmail.com>
2022-09-12 18:30:48 -0500

    initial commit

---
+++ MethodExercise/Method.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodExercise
+{
+    internal class Method
+
+    {
+        public static void Exercise1()
+        {
+
+            Console.WriteLine("What is your first name?");
+            var firstName = Console.ReadLine(); //input from user
+
+            Console.WriteLine("What is your favorite color?");
+            var favoriteColor = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite animal?");
+            var favoriteAnimal = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite number?");
+            var favoriteNumber = Console.ReadLine();
+
+            Console.WriteLine($"Hi {firstName}!  My favorite color is also {favoriteColor}.  " +
+            $"I love that your favorite animal is a {favoriteAnimal}.  I also just learned " +
+            $"that your favorite number is {favoriteNumber}.");
+        }
+
+
+
+        //Declare 5 methods: 1 Add method, 1 subtract method, 1 multiply method, 1 divide method, 1 modulus method.
+
+        //add method
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        //subtract method
+        public static int Subtract(int num1, int num2, int num3)
+        {
+            return num1 - num2 - num3;
+        }
+
+        //multiply method
+        public static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        //divide method
+        public static double Divide(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+
+        //modulus method
+        public static int Modulus(int num1, int num2)
+        {
+            return num1 % num2;
+        }
+
+        //Step 8 Bonus: Change the functions to us the params keyword to support a range of arguements.
+
+        public static int AddParams(params int[] parameterArrayNumbers)
+        {
+            int totalAdd = 0;
+            
+            foreach (int num in parameterArrayNumbers)
+            {
+                totalAdd += num;
+            }
+
+                return totalAdd; 
+
+               
+        }
+        
+
+            
+    }
+}
---
+++ MethodExercise/Program.cs
@@ -0,0 +1,51 @@
+﻿//C# Variables Exercise from TrueCoders portal
+//Record user input for Name, Favorite Color, Favorite Animal, Favorite Number
+// Part c -- *Bonus* Abstract away the code into methods and call them in your main
+using MethodExercise;
+Console.WriteLine("----------------------------Step 5c----------------------------------");
+
+Method.Exercise1();
+
+
+Console.WriteLine("----------------------------Step 6----------------------------------");
+//Declare 5 methods: 1 Add method, 1 subtract method, 1 multiply method, 1 divide method, 1 modulus method.
+
+//Add
+Console.Write($"Addition answer: ");
+Console.WriteLine(Method.Add(5, 5));
+
+
+//Subtract
+Console.Write($"Subtraction answer: ");
+Console.WriteLine(Method.Subtract(10, 5, 4));
+
+//Multiply
+Console.Write($"Multiplication answer: ");
+Console.WriteLine(Method.Multiply(3, 5));
+
+//Divide
+Console.Write($"Division answer: ");
+Console.WriteLine(Method.Divide(20, 3));
+
+//Modulus
+Console.Write($"Modulus answer: ");
+Console.WriteLine(Method.Modulus(42, 5));
+
+
+Console.WriteLine("----------------------------Bonus Step 8----------------------------------");
+//Change the functions to us the params keyword to support a range of arguements.
+//Add() methhod that takes as many arguements as needed
+
+//a. Sum(2,4) would return 6.  
+Console.Write($"Sum(2,4) answer: ");
+Console.WriteLine(Method.AddParams(2,4));
+
+//b. Sum (2,4,6) would return 12.
+Console.Write($"Sum(2,4,6) answer: ");
+Console.WriteLine(Method.AddParams(2, 4, 6));
+
+//c. Sum (1,1,1,1,1) would return 5
+Console.Write($"Sum (1,1,1,1,1) answer: ");
+Console.WriteLine(Method.AddParams(1, 1, 1, 1, 1));
+
+
